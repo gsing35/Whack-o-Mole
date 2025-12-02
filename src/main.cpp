1b3d3f9e51e24adefaac79fcc87d81081ffb4d65
gsing35 <gurpritsingh24680@gmail.com>
2025-12-02 12:51:50 -0500

    Refactor audio control and oscillator management
    Refactor audio control and oscillator handling for improved functionality.

--- src/main.cpp
+++ src/main.cpp
@@ -3,120 +3,92 @@
 #include <Oscil.h>
 #include <tables/sin2048_int8.h>
 
-#define CONTROL_RATE 64
-#define NUM_VOICES 8   
 
-const int pins[] = {22, 23, 26, 27, 30, 31, 34, 35, 38, 39, 42, 43};
-const int touchPinOctave = 2;
+Oscil<SIN2048_NUM_CELLS, AUDIO_RATE> oscs[12] = { // Array of mozzi oscilators to play every note at same time
+  {SIN2048_DATA}, {SIN2048_DATA}, {SIN2048_DATA},
+  {SIN2048_DATA}, {SIN2048_DATA}, {SIN2048_DATA},
+  {SIN2048_DATA}, {SIN2048_DATA}, {SIN2048_DATA},
+  {SIN2048_DATA}, {SIN2048_DATA}, {SIN2048_DATA} };
 
-bool lastOctButton = false;
-int octave = 1;
-bool keyPressed[12] = {false};
 
-int baseFreqs[] = {
+const int pins[] = {22, 23, 26, 27, 30, 31, 34, 35, 38, 39, 42, 43}; // A–G#
+const int octave_pin = 2;
+const int buzzer_pin = 11;
+
+
+unsigned long last_octave_time = 0;
+int base_freqs[] = {
   440, // A
-  466, // Bb
+  466, // Ab
   494, // B
-  523, // C
-  554, // Dd
+  523, // Bb
+  554, // C
   587, // D
-  622, // Eb
+  622, // Dd
   659, // E
   698, // F
-  740, // Gb
+  740, // Fb
   784, // G
-  831  // Ab
+  831  // Gb
 };
+bool key_pressed[12] = {false};
+int octave = 1;
 
-Oscil<SIN2048_NUM_CELLS, AUDIO_RATE> voices[NUM_VOICES] = {
-  Oscil<SIN2048_NUM_CELLS, AUDIO_RATE>(SIN2048_DATA),
-  Oscil<SIN2048_NUM_CELLS, AUDIO_RATE>(SIN2048_DATA),
-  Oscil<SIN2048_NUM_CELLS, AUDIO_RATE>(SIN2048_DATA),
-  Oscil<SIN2048_NUM_CELLS, AUDIO_RATE>(SIN2048_DATA),
-  Oscil<SIN2048_NUM_CELLS, AUDIO_RATE>(SIN2048_DATA),
-  Oscil<SIN2048_NUM_CELLS, AUDIO_RATE>(SIN2048_DATA),
-  Oscil<SIN2048_NUM_CELLS, AUDIO_RATE>(SIN2048_DATA),
-  Oscil<SIN2048_NUM_CELLS, AUDIO_RATE>(SIN2048_DATA)
-};
-
-float voiceFreq[NUM_VOICES] = {0};
-
-void noteOn(float freq) {
-  for (int i = 0; i < NUM_VOICES; i++) {
-    if (voiceFreq[i] == 0) {
-      voiceFreq[i] = freq;
-      voices[i].setFreq(freq);
-      return;
-    }
-  }
-}
-
-void noteOff(float freq) {
-  for (int i = 0; i < NUM_VOICES; i++) {
-    if (voiceFreq[i] == freq) {
-      voiceFreq[i] = 0;
-      voices[i].setFreq(0);
-      return;
-    }
-  }
-}
-
-void updateControl() {
-
-}
-
-AudioOutput_t updateAudio() {
-  int mixed = 0;
-
-  for (int i = 0; i < NUM_VOICES; i++) {
-    if (voiceFreq[i] > 0) {
-      mixed += voices[i].next();
-    }
-  }
-
-  mixed /= 1; 
-
-  return MonoOutput::from8Bit(mixed);
-}
 
 void setup() {
-  pinMode(touchPinOctave, INPUT_PULLUP);
-  for (int i = 0; i < 12; i++) pinMode(pins[i], INPUT_PULLUP);
-
+  pinMode(octave_pin, INPUT_PULLUP);
+  for (int i = 0; i < 12; i++) {
+    pinMode(pins[i], INPUT_PULLUP);
+  }
+  pinMode(buzzer_pin, OUTPUT);
   Serial.begin(115200);
-
-  startMozzi(CONTROL_RATE);
+  startMozzi();
 }
 
-void loop() {
-  if (digitalRead(touchPinOctave) == LOW && !lastOctButton) {
+
+void updateControl() {
+    // Run every 250ms
+    if (digitalRead(octave_pin) == HIGH && (millis() -last_octave_time) > 250) {
     octave = (octave == 1) ? 2 : 1;
     Serial.print("Switched to octave ");
     Serial.println(octave);
-    lastOctButton = true;
-  } else if (digitalRead(touchPinOctave) == HIGH) {
-    lastOctButton = false;
+    last_octave_time = millis();
   }
 
-  for (int i = 0; i < 12; i++) {
-    int freq = baseFreqs[i] * octave;
 
-    if (digitalRead(pins[i]) == LOW) {
-      if (!keyPressed[i]) {
-        keyPressed[i] = true;
-        noteOn(freq);
-        Serial.print("Note ON: ");
-        Serial.println(freq);
-      }
+  // Check if key pressed and stored which key
+  for (int i = 0; i < 12; i++) {
+    if (digitalRead(pins[i]) == HIGH) {  // key pressed
+      oscs[i].setFreq(base_freqs[i] * octave);
+      key_pressed[i] = true;
     } else {
-      if (keyPressed[i]) {
-        keyPressed[i] = false;
-        noteOff(freq);
-        Serial.print("Note OFF: ");
-        Serial.println(freq);
-      }
+      key_pressed[i] = false;
+    }
+  }
+}
+
+
+// Mix th audio
+int updateAudio() {
+  int mix_audio = 0;
+
+
+  for (int i = 0; i < 12; i++) {
+    if (key_pressed[i]) {
+      mix_audio += oscs[i].next();
     }
   }
 
+
+  return mix_audio >> 3;  // Right bit shift to avoid distored sounds
+}
+
+
+void loop() {
   audioHook();
 }
+
+
+
+
+
